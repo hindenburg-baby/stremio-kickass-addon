3037ceb796be50838908efb1d2ea0cbb0261339b
brocker brocker <brocker123@protonmail.com>
2023-05-31 06:25:29 +0300

    version bumped to 0.0.2

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.get('/stream/:type_/:videoid.json', (req, res) => {
 app.get('/manifest.json', (req, res) => {
   res.send({
     "id": "com.stremio-kickass-addon",
-    "version": "0.0.1",
+    "version": "0.0.2",
     "name": "Kickass Torrents Streams",
     "description": "Streams from Kickass Torrents",
     "types": [ "movie", "series" ],
